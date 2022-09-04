b7c6496d7bc203520a0737cf3432e7f57677bb6d
jacksonlmp <jacksonlmp17.jl@gmail.com>
2022-09-04 01:10:09 -0300

    Configurando rota

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,6 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/alunos/novo', 'App\Http\Controllers\AlunosController@create');
+Route::post('/alunos/novo', 'App\Http\Controllers\AlunosController@store')->name('cadastrar_aluno');
