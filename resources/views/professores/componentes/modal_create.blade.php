5a3dbcc61ed755beb9a7d0d594efb3c2ac82c65e
Luiz Davi <luizd4398@gmail.com>
2022-09-07 12:30:11 -0300

    wip listagem

--- resources/views/professores/componentes/modal_create.blade.php
+++ resources/views/professores/componentes/modal_create.blade.php
@@ -1,29 +1,29 @@
 <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
-  <div class="modal-dialog">
+  <div class="modal-dialog modal-dialog-centered">
     <div class="modal-content modal-create">
       <div class="modal-header" >
-        <h5 class="modal-title">Cadastro de Professor</h5>
-        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+        <h5 class="modal-title title" >Cadastro de Professor</h5>
+        <button class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
       </div>
-      <form action="{{route("professores.store")}}" method="post" >
+      <form action="{{route("professores.store")}}" method="post">
         @csrf
         <div class="modal-body">
-            <div class="mb-3" style="">
-              <label for="nome" class="form-label">Nome</label>
-              <input name="nome" class="form-control input-modal-create" type="text" placeholder="Digite o nome do professor" >
-            </div>
-
-            <div class="mb-3">
-              <label for="exampleInputEmail1" class="form-label">SIAPE</label>
-              <input name="siape" class="form-control input-modal-create" type="text" placeholder="Digite o SIAPE do professor">
-            </div>
-
-            <div class="mb-3">
+          <div class="mb-3" style="">
+            <label for="nome" class="form-label">Nome</label>
+            <input name="nome" class="form-control input-modal-create" type="text" placeholder="Digite o nome do professor" >
+          </div>
+          <div class="row">
+            <div class="col-sm- 12 col-md-6 mb-3">
               <label for="exampleInputEmail1" class="form-label">CPF</label>
               <input name="cpf" class="form-control input-modal-create" type="text" placeholder="Digite o CPF do professor">
             </div>
-            
-            <button type="submit" class="btn btn-primary submit-button" style="margin-top: 30px;">Cadastrar professor</button>
+              
+            <div class="col-sm- 12 col-md-6 mb-3">
+              <label for="exampleInputEmail1" class="form-label">SIAPE</label>
+              <input name="siape" class="form-control input-modal-create" type="text" placeholder="Digite o SIAPE do professor">
+            </div>
+          </div>
+          <button type="submit" class="btn btn-primary submit-button" style="margin-top: 30px;">Cadastrar professor</button>
         </div>
       </form>
     </div>
--- resources/views/professores/index.blade.php
+++ resources/views/professores/index.blade.php
@@ -1,15 +1,45 @@
 @extends("templates.app")
 
 @section("body")
-  <h1>Hey there</h1>
-
-  <!-- Button trigger modal -->
-  <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#exampleModal">
-    Launch demo modal
-  </button>
+  <div class="container">
+    <h1><strong>Professores</strong></h1>
+    <a type="button" data-bs-toggle="modal" data-bs-target="#exampleModal" style="text-align: left">
+      <img src="{{asset("images/add-icon.png")}}" class="add-button" alt="Adicionar Professor">
+    </a>
+  
+    @include("professores.componentes.modal_create")
+  
+    @if (sizeof($users) == 0)
+      <div class="empty">
+        <p>
+          Não há professores Cadastrados
+        </p>
+      </div>
+    @else
+      <div id="list">
+        @foreach ($users as $user)
+          <div class="listing-card">
+            <div class="row">
+              <div class="col-sm- 9 col-md-4 informacoes">
+                <p>
+                  {{$user->nome}} - {{$user->siape}}
+                </p>
+                <p>
+                  {{$user->cpf}}
+                </p>
+              </div>
+              <div class="col-sm- 3 col-md-2 opcoes">
+                <img src="{{asset("images/editar.png")}}" class="add-button" alt="Adicionar Professor">
+                <img src="{{asset("images/excluir.png")}}" class="add-button" alt="Adicionar Professor" >
+              </div>
+            </div>
+          </div>
+        @endforeach
+      </div>
+    @endif
+  </div>
+ 
 
   
-
-  @include("professores.componentes.modal_create")
 
 @endsection
