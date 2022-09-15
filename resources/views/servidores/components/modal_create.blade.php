dfa60370158c5e3d33e8bd465b90917dde1dc2d7
Thiago <tcavalcanti.tc@gmail.com>
2022-09-14 21:04:51 -0300

    Ajustando CSS do cadastro

--- resources/views/servidores/components/modal_create.blade.php
+++ resources/views/servidores/components/modal_create.blade.php
@@ -8,16 +8,17 @@
       <form action="{{route("servidores.store")}}" method="post">
         @csrf
         <div class="modal-body">
-          <div class="mb-3" style="">
-            <label for="nome" class="form-label">Nome</label>
-            <input name="nome" class="form-control input-modal-create" type="text" placeholder="Digite o nome" >
-          </div>
           <div class="row">
+            <div class="col-sm- 12 col-md-6 mb-3">
+              <label for="nome" class="form-label">Nome</label>
+              <input name="nome" class="form-control input-modal-create" type="text" placeholder="Digite o nome" >
+            </div>
             <div class="col-sm- 12 col-md-6 mb-3">
               <label for="cpf" class="form-label">CPF</label>
               <input name="cpf" class="form-control input-modal-create" type="text" placeholder="Digite o CPF">
             </div>
-              
+          </div>
+          <div class="row">
             <div class="col-sm- 12 col-md-6 mb-3">
               <label for="email" class="form-label">E-mail</label>
               <input name="email" class="form-control input-modal-create" type="text" placeholder="Digite o e-mail">
@@ -26,7 +27,6 @@
               <label for="password" class="form-label">Senha</label>
               <input name="password" class="form-control input-modal-create" type="password" placeholder="Digite a senha">
             </div>
-
           </div>
           <button type="submit" class="btn btn-primary submit-button" style="margin-top: 30px;">Cadastrar servidor</button>
         </div>
