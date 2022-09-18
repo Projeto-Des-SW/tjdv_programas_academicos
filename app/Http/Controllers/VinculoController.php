ae2326ab5b53e6947abc7e622a9d61f100b863ab
Luiz Davi <luizd4398@gmail.com>
2022-09-18 14:07:58 -0300

    criando barra de pesquisa

--- app/Http/Controllers/VinculoController.php
+++ app/Http/Controllers/VinculoController.php
@@ -10,15 +10,36 @@ use Illuminate\Http\Request;
 class VinculoController extends Controller
 {
 
-    public function index()
+    public function index(Request $request)
     {
         $professors = Professor::all();
         $alunos = Aluno::all();
-        $vinculos = Vinculo::all();
+
+        $search = $request->search;
+        $programa = $request->programa;
+
+        $vinculos = Vinculo::join("alunos", "vinculos.aluno_id", "=", "alunos.id")->join("professors", "vinculos.professor_id", "=", "professors.id");
+        $vinculos = $vinculos->where(function ($query) use ($search, $programa) {
+            if ($search) {
+                $query->where("alunos.nome", "LIKE", "%{$search}%");
+                $query->orWhere("alunos.cpf", "LIKE", "%{$search}%");
+                $query->orWhere("professors.nome", "LIKE", "%{$search}%");
+                $query->orWhere("professors.cpf", "LIKE", "%{$search}%");
+                $query->orWhere("professors.siape", "LIKE", "%{$search}%");
+                $query->orWhere("professors.siape", "LIKE", "%{$search}%");
+                $query->orWhere("vinculos.bolsa", "LIKE", "%{$search}%");
+            }
+
+            if ($programa) {
+                $programa = strtoupper($programa);
+                $query->where("vinculos.bolsa", "LIKE", "%{$search}%");
+            }
+        })->get();
+
         return view("vinculos.index", compact('vinculos', 'professors', 'alunos'));
     }
 
-    public function create (Request $request)
+    public function create(Request $request)
     {
         dd("FALTA FAZER");
     }
