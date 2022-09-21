b5aacf171fa94c99a6bb6d5fd2f2acee35563738
Thiago <tcavalcanti.tc@gmail.com>
2022-09-20 23:46:39 -0300

    Criando validação dos dados de servidor

--- app/Models/Servidor.php
+++ app/Models/Servidor.php
@@ -16,4 +16,21 @@ class Servidor extends Model
     public function user(){
         return $this->morphOne(User::class, "typage");
     }
+
+    public static $rules = [
+        'nome' => 'bail|required|min:10|max:100|regex:/^[a-zA-Z\']+(?:\s[a-zA-Z\']+)+$/',
+        'cpf' => 'bail|required|formato_cpf|cpf|unique:servidors|unique:alunos'
+    ];
+
+    public static $messages = [
+        'nome.required' => 'Nome é obrigatório',
+        'nome.min' => 'Nome deve possuir pelo menos 10 caracteres',
+        'nome.max' => 'Nome deve possuir no máximo 100 caracteres',
+        'nome.regex' => 'Nome deve conter apenas letras',
+        'cpf.required' => 'CPF é obrigatório',
+        'cpf.formato_cpf' => 'Padrão deve ser 999.999.999-99',
+        'cpf.cpf' => 'CPF inválido',
+        'cpf.unique' => 'CPF já cadastrado',
+    ];
+
 }
