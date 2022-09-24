1304bc98d6c7e97ec8feeb163564ff3d69a3f0cc
jacksonlmp <jacksonlmp17.jl@gmail.com>
2022-09-24 00:26:59 -0300

    Mudança de icone para vinculo

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -5,32 +5,32 @@
     <h1><strong>Programas</strong></h1>
     <div class="container">
         <button class="block">
-            <img src="{{asset("images/bolsa.png")}}" alt="Todas as bolsas"/>
+            <img src="{{asset("images/vinculo.png")}}" alt="Todas as bolsas"/>
             <h3 class="text-home">Todas as bolsas</h3>    
         </button>
 
         <button class="block">
-            <img src="{{asset("images/bolsa.png")}}" alt="Todas as bolsas"/>
+            <img src="{{asset("images/vinculo.png")}}" alt="Todas as bolsas"/>
             <h3 class="text-home">Monitorias</h3>
         </button>
 
         <button class="block">
-            <img src="{{asset("images/bolsa.png")}}" alt="Todas as bolsas"/>
+            <img src="{{asset("images/vinculo.png")}}" alt="Todas as bolsas"/>
             <h3 class="text-home">Tutorias</h3>
         </button>
 
         <button class="block">
-            <img src="{{asset("images/bolsa.png")}}" alt="Todas as bolsas"/>
+            <img src="{{asset("images/vinculo.png")}}" alt="Todas as bolsas"/>
             <h3 class="text-home">PAVI</h3>
         </button>
 
         <button class="block">
-            <img src="{{asset("images/bolsa.png")}}" alt="Todas as bolsas"/>
+            <img src="{{asset("images/vinculo.png")}}" alt="Todas as bolsas"/>
             <h3 class="text-home">BIA</h3>
         </button>
 
         <button class="block">
-            <img src="{{asset("images/bolsa.png")}}" alt="Todas as bolsas"/>
+            <img src="{{asset("images/vinculo.png")}}" alt="Todas as bolsas"/>
             <h3 class="text-home">PET</h3>
         </button>
     </div>
