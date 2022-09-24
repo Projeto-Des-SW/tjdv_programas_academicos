40da4e4db7ec7257a30d611b9a9814e7b7bd3db4
jacksonlmp <jacksonlmp17.jl@gmail.com>
2022-09-23 21:54:37 -0300

    Transformando as divs em button

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -4,34 +4,34 @@
     
     <h1><strong>Programas</strong></h1>
     <div class="container">
-        <div class="block">
+        <button class="block">
             <img src="{{asset("images/bolsa.png")}}" alt="Todas as bolsas"/>
             <h3 class="text-home">Todas as bolsas</h3>    
-        </div>
+        </button>
 
-        <div class="block">
+        <button class="block">
             <img src="{{asset("images/bolsa.png")}}" alt="Todas as bolsas"/>
             <h3 class="text-home">Monitorias</h3>
-        </div>
+        </button>
 
-        <div class="block">
+        <button class="block">
             <img src="{{asset("images/bolsa.png")}}" alt="Todas as bolsas"/>
             <h3 class="text-home">Tutorias</h3>
-        </div>
+        </button>
 
-        <div class="block">
+        <button class="block">
             <img src="{{asset("images/bolsa.png")}}" alt="Todas as bolsas"/>
             <h3 class="text-home">PAVI</h3>
-        </div>
+        </button>
 
-        <div class="block">
+        <button class="block">
             <img src="{{asset("images/bolsa.png")}}" alt="Todas as bolsas"/>
             <h3 class="text-home">BIA</h3>
-        </div>
+        </button>
 
-        <div class="block">
+        <button class="block">
             <img src="{{asset("images/bolsa.png")}}" alt="Todas as bolsas"/>
             <h3 class="text-home">PET</h3>
-        </div>
+        </button>
     </div>
 @endsection
