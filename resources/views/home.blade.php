d972d633d0a96be10a873b9d6202ed49def4a6dc
jacksonlmp <jacksonlmp17.jl@gmail.com>
2022-09-23 21:26:17 -0300

    Estrutura Home + CSS

---
+++ resources/views/home.blade.php
@@ -0,0 +1,37 @@
+@extends("templates.app")
+
+@section("body")
+    
+    <h1><strong>Programas</strong></h1>
+    <div class="container">
+        <div class="block">
+            <img src="{{asset("images/bolsa.png")}}" alt="Todas as bolsas"/>
+            <h3 class="text-home">Todas as bolsas</h3>    
+        </div>
+
+        <div class="block">
+            <img src="{{asset("images/bolsa.png")}}" alt="Todas as bolsas"/>
+            <h3 class="text-home">Monitorias</h3>
+        </div>
+
+        <div class="block">
+            <img src="{{asset("images/bolsa.png")}}" alt="Todas as bolsas"/>
+            <h3 class="text-home">Tutorias</h3>
+        </div>
+
+        <div class="block">
+            <img src="{{asset("images/bolsa.png")}}" alt="Todas as bolsas"/>
+            <h3 class="text-home">PAVI</h3>
+        </div>
+
+        <div class="block">
+            <img src="{{asset("images/bolsa.png")}}" alt="Todas as bolsas"/>
+            <h3 class="text-home">BIA</h3>
+        </div>
+
+        <div class="block">
+            <img src="{{asset("images/bolsa.png")}}" alt="Todas as bolsas"/>
+            <h3 class="text-home">PET</h3>
+        </div>
+    </div>
+@endsection
