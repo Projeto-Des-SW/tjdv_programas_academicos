e7ea37d5e7317f436808e5f0abe711a81a384e95
jacksonlmp <jacksonlmp17.jl@gmail.com>
2022-09-24 14:37:11 -0300

    Refatoração na view de home

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -4,32 +4,32 @@
     
     <h1><strong>Programas</strong></h1>
     <div class="container">
-        <button class="block">
+        <button class="block-programas">
             <img src="{{asset("images/vinculo.png")}}" alt="Todas as bolsas"/>
             <h3 class="text-home">Todas as bolsas</h3>    
         </button>
 
-        <button class="block">
+        <button class="block-programas">
             <img src="{{asset("images/vinculo.png")}}" alt="Todas as bolsas"/>
             <h3 class="text-home">Monitorias</h3>
         </button>
 
-        <button class="block">
+        <button class="block-programas">
             <img src="{{asset("images/vinculo.png")}}" alt="Todas as bolsas"/>
             <h3 class="text-home">Tutorias</h3>
         </button>
 
-        <button class="block">
+        <button class="block-programas">
             <img src="{{asset("images/vinculo.png")}}" alt="Todas as bolsas"/>
             <h3 class="text-home">PAVI</h3>
         </button>
 
-        <button class="block">
+        <button class="block-programas">
             <img src="{{asset("images/vinculo.png")}}" alt="Todas as bolsas"/>
             <h3 class="text-home">BIA</h3>
         </button>
 
-        <button class="block">
+        <button class="block-programas">
             <img src="{{asset("images/vinculo.png")}}" alt="Todas as bolsas"/>
             <h3 class="text-home">PET</h3>
         </button>
