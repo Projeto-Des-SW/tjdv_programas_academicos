76f6f489c4d0dc56372ed20cb9dacc6a5d3919b4
Thiago <tcavalcanti.tc@gmail.com>
2022-09-25 00:00:24 -0300

    Criando validação dos dados de professor

--- app/Models/Professor.php
+++ app/Models/Professor.php
@@ -10,4 +10,24 @@ class Professor extends Model
     use HasFactory;
 
     protected $fillable = array('nome', 'cpf', 'siape');
+
+    public static $rules = [
+        'nome' => 'bail|required|min:10|max:100|regex:/^[a-zA-Z\']+(?:\s[a-zA-Z\']+)+$/',
+        'cpf' => 'bail|required|formato_cpf|cpf|unique:servidors|unique:alunos|unique:professors',
+        'siape' => 'bail|required|min:7|max:7',
+    ];
+
+    public static $messages = [
+        'nome.required' => 'Nome é obrigatório',
+        'nome.min' => 'Nome deve possuir no mínimo 10 caracteres',
+        'nome.max' => 'Nome deve possuir no máximo 100 caracteres',
+        'nome.regex' => 'Nome deve conter apenas letras',
+        'cpf.required' => 'CPF é obrigatório',
+        'cpf.formato_cpf' => 'Padrão deve ser 999.999.999-99',
+        'cpf.cpf' => 'CPF inválido',
+        'cpf.unique' => 'CPF já cadastrado',
+        'siape.required' => 'Siape é obrigatório',
+        'siape.min' => 'Siape deve possuir 7 caracteres',
+        'siape.max' => 'Siape deve possuir 7 caracteres'
+    ];
 }
