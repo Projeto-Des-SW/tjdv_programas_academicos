da9231140be357d084d8057dab4c464af369e415
Thiago <tcavalcanti.tc@gmail.com>
2022-09-25 22:16:50 -0300

    Criando regras de validação no model de aluno

--- app/Models/Aluno.php
+++ app/Models/Aluno.php
@@ -9,7 +9,6 @@ use App\Models\User;
 class Aluno extends Model
 {
     protected $fillable = [
-        'nome',
         'cpf',
         'curso',
         'semestre_entrada',
@@ -24,4 +23,21 @@ class Aluno extends Model
     {
         return $this->hasMany(Vinculo::class);
     }
+
+    public static $rules = [
+        'cpf' => 'bail|required|formato_cpf|cpf|unique:servidors|unique:alunos',
+        'curso' => 'bail|required|max:50',
+        'semestre_entrada' => 'bail|required|min:6|max:6',
+    ];
+
+    public static $messages = [
+        'cpf.required' => 'CPF é obrigatório',
+        'cpf.formato_cpf' => 'Padrão deve ser 999.999.999-99',
+        'cpf.cpf' => 'CPF inválido',
+        'cpf.unique' => 'CPF já cadastrado',
+        'curso.required' => 'Curso é obrigatório',
+        'curso.max' => 'Curso deve possuir no máximo 50 caracteres',
+        'semestre_entrada.required' => 'Semestre de entrada é obrigatório',
+        'semestre_entrada.max' => 'Semestre de entrada deve possuir 6 caracteres',
+    ];
 }
