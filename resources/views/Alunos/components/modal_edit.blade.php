e66901e17e35a3d963e1c7642873a00fa42689f7
Thiago <tcavalcanti.tc@gmail.com>
2022-09-26 20:25:45 -0300

    Exibindo erros de validação na view de editar aluno

--- resources/views/Alunos/components/modal_edit.blade.php
+++ resources/views/Alunos/components/modal_edit.blade.php
@@ -1,48 +1,96 @@
-<div class="modal fade" id="editModal" tabindex="-1" aria-hidden="true">
+<div data-backdrop="static" data-keyboard="false" role="dialog" class="modal fade" id="modal_edit_{{$aluno->id}}" tabindex="-1" aria-hidden="true">
   <div class="modal-dialog modal-dialog-centered">
     <div class="modal-content modal-create">
       <div class="modal-header" >
-        <h5 class="modal-title title" >Editar Aluno</h5>
-        <button class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+        <h5 class="modal-title title">Editar aluno</h5>
+        <button class="btn-close" data-bs-dismiss="modal" onClick="window.location.reload();" aria-label="Close"></button>
       </div>
-      <form id="form-edit" action="{{route('alunos.update')}}" method="post">
+      <form action="{{route('alunos.update')}}" method="post">
         @csrf
-        <input type="hidden" name="id_edit" id="id_edit">
+        <input type="hidden" name="id" value="{{$aluno->id}}">
         <div class="modal-body">
           <div class="mb-3" style="">
             <label for="nome_edit" class="form-label">Nome</label>
-            <input name="nome_edit"id="nome_edit" class="form-control input-modal-create" type="text" placeholder="Digite o nome" >
+            <input name="name" type="text" placeholder="Digite o nome" value="{{old('name', $aluno->user->name)}}"
+            class="form-control input-modal-create @if(!empty($errors->update->first('name'))) is-invalid @endif">
+            @if(!empty($errors->update->first('name')))
+                  <span class="invalid-feedback d-block">
+                    <strong> {{$errors->update->first('name')}} </strong>
+                  </span>
+            @endif
           </div>
 
           <div class="row">
             <div class="col-sm- 12 col-md-6 mb-3">
               <label for="cpf_edit" class="form-label">CPF</label>
-              <input name="cpf_edit" id="cpf_edit" class="form-control input-modal-create" type="text" placeholder="Digite o CPF">
+              <input name="cpf" type="text" placeholder="Digite o CPF" value="{{old('cpf', $aluno->cpf)}}"
+              class="form-control input-modal-create @if(!empty($errors->update->first('cpf'))) is-invalid @endif">
+              @if(!empty($errors->update->first('cpf')))
+                  <span class="invalid-feedback d-block">
+                    <strong> {{$errors->update->first('cpf')}} </strong>
+                  </span>
+              @endif
             </div>
             
             <div class="col-sm- 12 col-md-6 mb-3">
               <label for="curso_edit" class="form-label">Curso</label>
-              <input name="curso_edit" id="curso_edit" class="form-control input-modal-create" type="text" placeholder="Digite o curso">
+              <input name="curso" type="text" placeholder="Digite o curso" value="{{old('curso', $aluno->curso)}}"
+              class="form-control input-modal-create @if(!empty($errors->update->first('curso'))) is-invalid @endif">
+              @if(!empty($errors->update->first('curso')))
+                  <span class="invalid-feedback d-block">
+                    <strong> {{$errors->update->first('curso')}} </strong>
+                  </span>
+              @endif
             </div> 
 
             <div class="col-sm- 12 col-md-6 mb-3">
               <label for="email_edit" class="form-label">E-mail</label>
-              <input name="email_edit" id="email_edit" class="form-control input-modal-create" type="text" placeholder="Digite o email">
+              <input name="email" type="text" placeholder="Digite o email" value="{{old('email', $aluno->user->email)}}"
+              class="form-control input-modal-create @if(!empty($errors->update->first('email'))) is-invalid @endif">
+              @if(!empty($errors->update->first('email')))
+                  <span class="invalid-feedback d-block">
+                    <strong> {{$errors->update->first('email')}} </strong>
+                  </span>
+              @endif
             </div> 
 
             <div class="col-sm- 12 col-md-6 mb-3">
               <label for="senha_edit" class="form-label">Senha</label>
-              <input name="senha_edit" id="senha_edit" class="form-control input-modal-create" type="password" placeholder="Digite a senha">
+              <input name="password" type="password" placeholder="Digite a senha"
+              class="form-control input-modal-create @if(!empty($errors->update->first('password'))) is-invalid @endif">
+              @if(!empty($errors->update->first('password')))
+                  <span class="invalid-feedback d-block">
+                    <strong> {{$errors->update->first('password')}} </strong>
+                  </span>
+              @endif
             </div> 
 
             <div class="col-sm- 12 col-md-6 mb-3">
               <label for="semestre_entrada_edit" class="form-label">Semestre de entrada</label>
-              <input name="semestre_entrada_edit" id="semestre_entrada_edit" class="form-control input-modal-create" type="text" placeholder="Digite o semestre de entrada">
+              <input name="semestre_entrada" type="text" placeholder="Digite o semestre de entrada" value="{{old('semestre_entrada', $aluno->semestre_entrada)}}"
+              class="form-control input-modal-create @if(!empty($errors->update->first('semestre_entrada'))) is-invalid @endif">
+              @if(!empty($errors->update->first('semestre_entrada')))
+                  <span class="invalid-feedback d-block">
+                    <strong> {{$errors->update->first('semestre_entrada')}} </strong>
+                  </span>
+              @endif
             </div>
           </div>
-          <button id="submit_edit" type="submit" class="btn btn-primary submit-button" style="margin-top: 30px;">Salvar informações</button>
+          <button type="submit" class="btn btn-primary submit-button" style="margin-top: 30px;">Salvar informações</button>
         </div>
       </form>
     </div>
   </div>
 </div>
+
+<script type="text/javascript">
+    $(function () {
+      $('#cpf_edit').mask('000.000.000-00');
+    });
+</script>
+
+<script type="text/javascript">
+    $(function () {
+      $('#semestre_entrada').mask('0000.0');
+    });
+</script>
