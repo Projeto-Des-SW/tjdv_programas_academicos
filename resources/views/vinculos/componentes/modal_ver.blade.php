fb58617e5df9ae47f199eaab88428d3511c244a3
Thiago <tcavalcanti.tc@gmail.com>
2022-10-04 21:55:29 -0300

    Adicionando acento e substituindo maiúscula no meio de frase

--- resources/views/vinculos/componentes/modal_ver.blade.php
+++ resources/views/vinculos/componentes/modal_ver.blade.php
@@ -56,7 +56,7 @@
           </div>
         
           <div style="margin-bottom: 15px; margin-top: 15px">
-            <a id="frequencia_mensal" class="btn btn-success" role="button" style="width: 230px">Formulário de Frequência</a>
+            <a id="frequencia_mensal" class="btn btn-success" role="button" style="width: 230px">Formulário de frequência</a>
             <p></p>
             <a class="btn btn-primary submit-button" data-bs-dismiss="modal" style="width: 230px" role="button">Voltar</a>
           </div> 
