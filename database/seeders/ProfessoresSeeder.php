d43a761de1ce604e8139b090c2cabeb48db3db56
Luiz Davi <luizd4398@gmail.com>
2022-10-10 10:37:10 -0300

    adicionando email ao professor (apenas no banco)

--- database/seeders/ProfessoresSeeder.php
+++ database/seeders/ProfessoresSeeder.php
@@ -11,30 +11,35 @@ class ProfessoresSeeder extends Seeder
     {
         Professor::create([
             'nome' => 'Rodrigo Gusmão',
+            'email' => "luizd4398@gmal.com",
             'cpf' => '996.889.168-11',
             'siape' => '1234567'
         ]);
 
         Professor::create([
             'nome' => 'Marcius',
+            'email' => "aaaa@gmal.com",
             'cpf' => '017.197.098-58',
             'siape' => '1234566'
         ]);
 
         Professor::create([
             'nome' => 'Thais Burity',
+            'email' => "bbbbb@gmal.com",
             'cpf' => '699.493.378-44',
             'siape' => '1234565'
         ]);
 
         Professor::create([
             'nome' => 'Ícaro Lins',
+            'email' => "ccccc@gmal.com",
             'cpf' => '107.409.038-10',
             'siape' => '1234564'
         ]);
 
         Professor::create([
             'nome' => 'Jean Carlos',
+            'email' => "asssss@gmal.com",
             'cpf' => '910.096.198-10',
             'siape' => '1234563'
         ]);
